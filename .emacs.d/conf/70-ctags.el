b1779e1ba363db06bb94f92bba346ba61897b61f
kaz.oji <zil@lunchbox3.(none)>
2012-04-02 23:27:42 +0900

    added anything-exuberant-ctags.el

--- .emacs.d/conf/70-ctags.el
+++ .emacs.d/conf/70-ctags.el
@@ -1,5 +1,9 @@
 ;;; --- ctags ---
 (require 'ctags nil t)
 (setq tags-revert-without-query t)
-(setq ctags-command "ctags -e -R ")
-(global-set-key (kbd "<f5>") 'ctags-create-or-update-tags-table)
+;;(setq ctags-command "ctags -e -R ")
+(setq ctags-command "ctags --verbose -R --fields=\"+afikKlmnsSzt\" ")
+(global-set-key (kbd "<f5>") 'ctags-create-or-update-tags-table) 
+
+(when (require 'anything-exuberant-ctags nil t)
+  (global-set-key (kbd "<C-f10>") 'anything-exuberant-ctags-select-from-here))
