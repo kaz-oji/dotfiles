c65d98a26740faa368a61d6a267bc96cbd221b01
kaz.oji <kaz.oji@gmail.com>
2012-05-10 21:54:15 +0900

    Add keybind C-tab to change tab on Elscreen.

--- .emacs.d/conf/53-elscreen.el
+++ .emacs.d/conf/53-elscreen.el
@@ -1,4 +1,7 @@
 ;; ElScreen
 (setq elscreen-prefix-key (kbd "C-t"))
 (when (require 'elscreen nil t)
-  (require 'elscreen-howm nil t))
+  (require 'elscreen-howm nil t)
+  (global-set-key [(C-tab)] 'elscreen-next)
+;  (global-set-key [(C-S-tab)] 'elscreen-previous)
+  )
